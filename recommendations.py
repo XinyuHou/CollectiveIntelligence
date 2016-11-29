f91336136c15440cc9974c357ed7d18de48cff83
Xinyu Hou <xinyu@boltonsoftware.com>
2016-11-29 21:36:09 +0000

    Add get recommendation according to weighted ranking

--- recommendations.py
+++ recommendations.py
@@ -122,4 +122,37 @@ def topMatches(prefs, person, n = 5, similarity = sim_pearson) :
 
     return scores[0 : n]
 
+# Gets recommendations for a person by using a weighted average of every other user's rankings
+def getRecommendations(prefs, person, similarity = sim_pearson) :
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        if other == person :
+            continue
+
+        sim = similarity(prefs, person, other)
+
+        if sim <= 0 :
+            continue
+
+        for item in prefs[other] :
+            # Only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0 :
+                # similarity * score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+        # Create the normalized list
+        rankings = [(total / simSums[item], item) for item, total in totals.items()]
+
+        # Return the sorted list
+        rankings.sort()
+        rankings.reverse()
+
+        return rankings
+
     
