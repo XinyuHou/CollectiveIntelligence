562c46047a3ba5f4abf10415668ca769dcb4b230
Xinyu Hou <xinyu@boltonsoftware.com>
2016-11-29 21:05:22 +0000

    Add pearson correlation

--- recommendations.py
+++ recommendations.py
@@ -76,3 +76,39 @@ def sim_distance(prefs, person1, person2) :
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2) for item in si])
 
     return 1 / (1 + sqrt(sum_of_squares))
+
+# Returns the Pearson correlation coefficient for p1 and p2
+def sim_pearson(prefs, person1, person2) :
+    # Get the list of shared_items
+    si = {}
+    for item in prefs[person1] :
+        if item in prefs[person2] :
+            si[item] = 1
+
+    n = len(si)
+    # If they have no rating s in common return 0
+    if n == 0 :
+        return 0
+
+    # Add up all the preferences
+    sum1 = sum([prefs[person1][item] for item in si])
+    sum2 = sum([prefs[person2][item] for item in si])
+
+    # Sum up the squares
+    sum1Sq = sum([pow(prefs[person1][item], 2) for item in si])
+    sum2Sq = sum([pow(prefs[person2][item], 2) for item in si])
+
+    # Sum up the products
+    productSum = sum([prefs[person1][item] * prefs[person2][item] for item in si])
+
+    # Calculate Pearson score
+    num = productSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0 :
+        return 0
+
+    r = num / den
+
+    return r
+
+    
