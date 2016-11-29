93d0759005a98ab9718e81e2c4b4d9a2d464e616
Xinyu Hou <xinyu@boltonsoftware.com>
2016-11-29 22:28:56 +0000

    Change dataset and add item based recommendation

--- recommendations.py
+++ recommendations.py
@@ -3,10 +3,10 @@ from math import sqrt
 # A dictionary of movie critics and their ratings of a small set of movie
 critics = {
     'Lisa Rose' : {
-        'Lady in the Water' : 2.5,
+        'Lady in the Water' : 1.5,
         'Snakes on a Plane' : 3.5,
         'Just My Luck' : 3.0,
-        'Superman Returns' : 3.5,
+        'Superman Returns' : 3.2,
         'You, Me and Dupree' : 2.5,
         'The Night Listener' : 3.0
     },
@@ -20,25 +20,25 @@ critics = {
         'The Night Listener' : 3.0
     },
 
-    'Michael Philips' : {
+    'Michael Phillips' : {
         'Lady in the  Water' : 2.5,
-        'Snakes on a Plane' : 3.0,
+        'Snakes on a Plane' : 3.5,
         'Just My Luck' : 3.0,
-        'Superman Returns' : 3.5,
+        'Superman Returns' : 1.5,
         'The Night Listener' : 4.0
     },
 
     'Claudia Puig' : {
         'Snakes on a Plane' : 3.5,
         'Just My Luck' : 3.0,
-        'Superman Returns' : 4.0,
+        'Superman Returns' : 2.0,
         'You, Me and Dupree' : 2.5,
         'The Night Listener' : 4.5
     },
 
     'Mike LaSalle' : {
         'Lady in the  Water' : 3.0,
-        'Snakes on a Plane' : 4.0,
+        'Snakes on a Plane' : 5.0,
         'Just My Luck' : 2.0,
         'Superman Returns' : 3.0,
         'You, Me and Dupree' : 2.0,
@@ -185,5 +185,39 @@ def calculateSimilarItems(prefs, n = 10) :
         result[item] = scores
 
     return result
-    
+
+def getRecommendedItems(prefs, itemMatch, user) :
+    userRatings = prefs[user]
+    scores = {}
+    totalSim = {}
+
+    # Loop over items rated by this user
+    for (item, rating) in userRatings.items() :
+
+        # Loop over items similar to this one
+        for (similarity, item2) in itemMatch[item]:
+
+            # Ignore if this user has already rated this item
+            if item2 in userRatings:
+                continue
+
+            # Weighted sum of rating times similarity
+            scores.setdefault(item2, 0)
+            scores[item2] += similarity * rating
+
+            # Sum of all the similarities
+            totalSim.setdefault(item2, 0)
+            totalSim[item2] += similarity
+
+        print totalSim
+        print scores
+        # Divide each total score by total weighting to get an average
+        rankings = [(score / totalSim[item], item) for item, score in scores.items()]
+
+        # Return the rankings from highest to lowest
+        rankings.sort()
+        rankings.reverse()
+
+        return rankings
+
 
