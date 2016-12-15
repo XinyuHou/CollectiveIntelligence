4781095b76ccc8facbccfb3d6bbe96ab8f4881ef
Xinyu Hou <xinyu@boltonsoftware.com>
2016-12-15 23:07:13 +0000

    Add Dendrogram drawing function

--- Clusters.py
+++ Clusters.py
@@ -103,3 +103,62 @@ def printClust(clust, labels = None, n = 0):
 	if clust.left != None: printClust(clust.left, labels = labels, n = n + 1)
 	if clust.right != None: printClust(clust.right, labels = labels, n = n + 1)
 
+from PIL import Image, ImageDraw
+
+def getHeight(clust):
+	# Is this an endpoint, if so the height is just 1
+	if clust.left == None and clust.right == None: return 1
+
+	# Otherwise the height is the same of the heights of each branch
+	return getHeight(clust.left) + getHeight(clust.right)
+
+def getDepth(clust):
+	# The distance of an endpoint is 0.0
+	if clust.left == None and clust.right == None: return 0
+
+	# The depth of a branch is the greater of its two sides plus its own distance
+	return max(getDepth(clust.left), getDepth(clust.right)) + clust.distance
+
+def drawDendrogram(clust, labels, bmp = 'Clusters.bmp'):
+	# Height and width
+	h = getHeight(clust) * 20
+	w = 1200
+
+	depth = getDepth(clust)
+
+	# Width is fixes, so scale distances accordingly
+	scaling = float(w - 150) / depth
+
+	# Create a new image with a white background
+	img = Image.new('RGB', (w, h), (255, 255, 255))
+	draw = ImageDraw.Draw(img)
+
+	# Draw the first node
+	drawNode(draw, clust, 10, (h / 2), scaling, labels)
+	img.save(bmp)
+
+def drawNode(draw, clust, x, y, scaling, labels):
+	if clust.id < 0:
+		h1 = getHeight(clust.left) * 20
+		h2 = getHeight(clust.right) * 20
+		top = y - (h1 + h2) / 2
+		bottom = y + (h1 + h2) / 2
+		# Line length
+		ll = clust.distance * scaling
+		# Vertical line from this cluster to children
+		draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill = (255, 0, 0))
+
+		# Horizontal line to left item
+		draw.line((x, top + h1 / 2, x + ll, top + h1 / 2), fill = (255, 0, 0))
+
+		# Horizontal line to right item
+		draw.line((x, bottom - h2 / 2, x + ll, bottom - h2 / 2), fill = (255, 0, 0))
+
+		# Call the function to draw the left and right nodes
+		drawNode(draw, clust.left, x + ll, top + h1 / 2, scaling, labels)
+		drawNode(draw, clust.right, x + ll, bottom - h2 / 2, scaling, labels)
+
+	else:
+		# If this is an endpoint, draw the item label
+		draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
