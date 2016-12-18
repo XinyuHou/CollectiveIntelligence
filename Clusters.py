7de056d779e6c132b6cd6031de56bc8fac4b6e07
Xinyu Hou <xinyu@boltonsoftware.com>
2016-12-18 22:04:27 +0000

    Add multi dementianal scaling and draw final coords into a pic

--- Clusters.py
+++ Clusters.py
@@ -225,4 +225,65 @@ def tanimoto(v1, v2):
 		if v1[i] != 0 and v2[i] != 0: share += 1
 
 	return 1.0 - (float(share) / (c2 + c2 - share))
-	
+
+# Multidimentional scaling
+def scaleDown(data, distance = pearson, rate = 0.01):
+	n = len(data)
+
+	# The real distances between every pair of items
+	realDist = [[distance(data[i], data[j]) for j in range(n)] for i in range(0, n)]
+	outerSum = 0.0
+
+	# Randomly initialize the starting points of the locations in 2D
+	loc = [[random.random(), random.random()] for i in range(n)]
+	fakeDist = [[0.0 for j in range(n)] for i in range(n)]
+
+	lastError = None
+	for m in range(0, 1000):
+		# Find projected distances
+		for i in range(n):
+			for j in range(n):
+				fakeDist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2) for x in range(len(loc[i]))]))
+
+		# Move points
+		grad = [[0.0, 0.0] for i in range(n)]
+
+		totalError = 0
+		for k in range(n):
+			for j in range(n):
+				if k == j: continue
+				# The error is percent difference between the distances
+				errorTerm = 0
+				if realDist[j][k] == 0:
+					errorTerm = 0
+				else:
+					errorTerm = (fakeDist[j][k] - realDist[j][k]) / realDist[j][k]
+
+				# Each point needs to be moved away from or towards the other point in proportion to thow much error it has
+				grad[k][0] += ((loc[k][0] - loc[j][0]) / fakeDist[j][k]) * errorTerm
+				grad[k][1] += ((loc[k][1] - loc[j][1]) / fakeDist[j][k]) * errorTerm
+
+				totalError += abs(errorTerm)
+
+		print totalError
+
+		# If the answer got worse by moving the points we are done
+		if lastError and lastError < totalError: break
+		lastError = totalError
+
+		# Move each of the points by the learning rate times the gradient
+		for k in range(n):
+			loc[k][0] -= rate * grad[k][0]
+			loc[k][1] -= rate * grad[k][1]
+
+	return loc
+
+def draw2D(data, labels, bmp='MultDimen2D.bmp'):
+	img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+	draw = ImageDraw.Draw(img)
+	for i in range(len(data)):
+		x = (data[i][0] + 0.5) * 1000
+		y = (data[i][1] + 0.5) * 1000
+		draw.text((x, y), labels[i], (0, 0, 0))
+
+	img.save(bmp)
