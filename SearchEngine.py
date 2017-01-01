e4f16e176a026b6ba774b6dd6ed32ea0fe2871d0
Xinyu Hou <xinyu@boltonsoftware.com>
2017-01-01 23:22:49 +0000

    Add basic searcher

--- SearchEngine.py
+++ SearchEngine.py
@@ -171,3 +171,38 @@ class Searcher:
 
 	def __del__(self):
 		self.db.close()
+
+	def getMatchRows(self, query):
+		# Strings to build the query
+		fieldList = 'w0.urlId'
+		tableList = ''
+		clauseList = ''
+		wordIds = []
+
+		# Split the words by spaces
+		words = query.split(' ')
+		tableNumber = 0
+
+		for word in words:
+			# Get the word ID
+			wordRow = self.db.execute("select rowid from WordList where word = '%s'" % word).fetchone()
+			if wordRow != None:
+				wordId = wordRow[0]
+				wordIds.append(wordId)
+				if tableNumber > 0:
+					tableList += ','
+					clauseList += ' and '
+					clauseList += 'w%d.urlId = w%d.urlId and ' % (tableNumber - 1, tableNumber)
+
+				fieldList += ', w%d.location' % tableNumber
+				tableList += 'WordLocation w%d' % tableNumber
+				clauseList += 'w%d.wordId = %d' % (tableNumber, wordId)
+				tableNumber += 1
+
+		# Create the query from the separate parts
+		fullQuery = 'select %s from %s where %s' % (fieldList, tableList, clauseList)
+
+		cur = self.db.execute(fullQuery)
+		rows = [row for row in cur]
+
+		return rows, wordIds
