45c890673f22746ef897a06074020ce3b1badbca
Xinyu Hou <xinyu@boltonsoftware.com>
2017-01-04 21:53:50 +0000

    Add generate hidden node if not exist

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,7 +9,7 @@ class SearchNet:
 		self.db.close()
 
 	def makeTables(self):
-		self.db.execute('create table HiddenNode(create_key)')
+		self.db.execute('create table HiddenNode(createKey)')
 		self.db.execute('create table WordHidden(fromId, toId, strength)')
 		self.db.execute('create table HiddenUrl(fromId, toId, strength)')
 
@@ -34,10 +34,31 @@ class SearchNet:
 		else:
 			table = 'HiddenUrl'
 
-		res = self.db.execute('select rowid from %s where fromId = %d amd toId = %d' % (table, fromId, toId)).fetchone()
+		res = self.db.execute('select rowid from %s where fromId = %d and toId = %d' % (table, fromId, toId)).fetchone()
 		if res == None:
 			self.db.execute('insert into %s (fromId, toId, strength) values (%d, %d, %f)' % (table, fromId, toId, strength))
 		else:
 			rowid = res[0]
 			self.db.execute('update %s set strength = %f where rowid = %d' % (table, strength, rowid))
-	
+
+	def generateHiddenNode(self, wordIds, urls):
+		if len(wordIds) > 3: return None
+
+		# Check if we alreayd created a node for this set of words
+		createKey = '_'.join(sorted([str(wi) for wi in wordIds]))
+		
+		res = self.db.execute("select rowid from HiddenNode where createKey = '%s'" % createKey).fetchone()
+
+		# If not, create it
+		if res == None:
+			cur = self.db.execute("insert into HiddenNode (createKey) values ('%s')" % createKey)
+			hiddenId = cur.lastrowid
+
+			# Put in some default weights
+			for wordId in wordIds:
+				self.setStrength(wordId, hiddenId, 0, 1.0 / len(wordIds))
+			
+			for urlId in urls:
+				self.setStrength(hiddenId, urlId, 1, 0.1)
+
+			self.db.commit()
