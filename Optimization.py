399ee4936363dae9491c7c8d8c8673c9749a8468
Xinyu Hou <xinyu@boltonsoftware.com>
2017-03-28 00:08:35 +0100

    Add hill climbing

--- Optimization.py
+++ Optimization.py
@@ -84,3 +84,32 @@ def randomOptimize(domain, costf):
 			bestr = r
 
 	return bestr
+
+def hillClimb(domain, costf):
+	sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+
+	while 1:
+		neighbors=[]
+
+		for j in range(len(domain)):
+			if sol[j] > domain[j][0]:
+				neighbors.append(sol[0 : j] + [sol[j] - 1] + sol[j + 1 :])
+			if sol[j] < domain[j][0]:
+				neighbors.append(sol[0 : j] + [sol[j] + 1] + sol[j + 1 :])
+
+		current = costf(sol)
+		best = current
+
+		for j in range(len(neighbors)):
+			cost = costf(neighbors[j])
+			if cost < best:
+				best = cost
+				sol = neighbors[j]
+
+		if best == current:
+			break
+
+	return sol
+
+
+
