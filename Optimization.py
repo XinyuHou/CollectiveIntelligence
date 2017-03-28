8e97b3a0b062494eeec72f19f7dfe09a90151a40
Xinyu Hou <xinyu@boltonsoftware.com>
2017-03-28 23:20:07 +0100

    Add annealling optimization

--- Optimization.py
+++ Optimization.py
@@ -111,5 +111,36 @@ def hillClimb(domain, costf):
 
 	return sol
 
+def annealingOptimize(domain, costf, T = 10000.0, cool = 0.95, step = 1):
+	# Initialize the values randomly
+	vec = [float(random.randint(domain[i][0], domain[i][1])) for i in range(len(domain))]
+	while T > 0.1:
+		# Choose one of the indices
+		i = random.randint(0, len(domain) - 1)
 
+		# Choose a direction to change it
+		dir = step * (-1) ** int(round(random.random()))
 
+		# New list with one of the values changed
+		vecb = vec[:]
+		vecb[i] += dir
+		if vecb[i] < domain[i][0]:
+			vecb[i] = domain[i][0]
+		elif vecb[i] > domain[i][1]:
+			vecb[i] = domain[i][1]
+
+		# Calculate the current cost and the new cost
+		cc = costf(vec)
+		nc = costf(vecb)
+
+		if nc < cc:
+			vec = vecb
+		else:
+			p = pow(math.e, (-nc - cc) / T)
+			if random.random() < p:
+				vec = vecb
+
+		# Decrease the temperature
+		T = T * cool
+
+	return vec
