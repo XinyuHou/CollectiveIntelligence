eba1d51eaa0fb0f0957976bcf0eeb4a262c9b0f6
Xinyu Hou <xinyu@boltonsoftware.com>
2017-03-30 22:14:49 +0100

    Add draw network

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,5 +1,5 @@
 import math
-
+from PIL import Image, ImageDraw
 people = ['Charlie', 'Augustus', 'Veruca', 'Violet', 'Mike', 'Joe', 'Willy', 'Miranda']
 
 links = [
@@ -32,15 +32,27 @@ def crossCount(v):
 			(x3, y3), (x4, y4) = loc[links[j][0]], loc[links[j][1]]
 
 			den = (y4 -y3) * (x2 - x1) - (x4 -x3) * (y2 - y1)
+
 			if den == 0:
 				continue
 
-			ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / den
-			ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / den
-
+			ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / float(den)
+			ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / float(den)
 			if ua > 0 and ua < 1 and ub > 0 and ub < 1:
-				total += 1
+				total += 1	
 
 	return total
 
+def drawNetwork(sol):
+	img = Image.new('RGB', (400, 400), (255, 255, 255))
+	draw = ImageDraw.Draw(img)
 
+	pos = dict([(people[i], (sol[i * 2], sol[i * 2 + 1])) for i in range(0, len(people))])
+
+	for (a, b) in links:
+		draw.line((pos[a], pos[b]), fill = (255, 0, 0))
+
+	for n, p in pos.items():
+		draw.text(p, n, (0, 0, 0))
+
+	img.show()
