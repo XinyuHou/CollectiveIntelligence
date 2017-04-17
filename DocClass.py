e1ffa33b88f3f4c24ef58b68cc214c2dba41b761
Xinyu Hou <xinyu@boltonsoftware.com>
2017-04-17 13:40:41 +0100

    Add classifier class

--- DocClass.py
+++ DocClass.py
@@ -7,3 +7,43 @@ def getWords(doc):
 	words = [s.lower() for s in splitter.split(doc) if len(s) > 2 and len(s) < 20]
 
 	return dict([(w, 1) for w in words])
+
+class classifier:
+	def __init__(self, getFeatures, filename = None):
+		self.featureCatCount = {}
+		self.catCount = {}
+		self.getFeatures = getFeatures
+
+	def incFeatureCatCount(self, f, cat):
+		self.featureCatCount.setdefault(f, {})
+		self.featureCatCount[f].setdefault(cat, 0)
+		self.featureCatCount[f][cat] += 1
+
+	def incCatCount(self, cat):
+		self.catCount.setdefault(cat, 0)
+		self.catCount[cat] += 1
+
+	def featureCount(self, f, cat):
+		if f in self.featureCatCount and cat in self.featureCatCount[f]:
+			return float(self.featureCatCount[f][cat])
+		return 0.0
+
+	def itemCount(self, cat):
+		if cat in self.catCount:
+			return float(self.catCount[cat])
+		return 0
+
+	def totalItemCount(self):
+		return sum(self.catCount.values())
+
+	def categories(self):
+		return self.catCount.keys()
+
+	def train(self, item, cat):
+		features = self.getFeatures(item)
+
+		for f in features:
+			self.incFeatureCatCount(f, cat)
+
+		self.incCatCount(cat)
+		
