4d20b4f2e4c7fd6320053979c0940fd829adb3e1
Xinyu Hou <xinyu@boltonsoftware.com>
2017-04-24 22:49:08 +0100

    Add gini impurity

--- TreePredict.py
+++ TreePredict.py
@@ -30,3 +30,21 @@ def uniqueCounts(rows):
 		results[r] += 1
 
 	return results
+
+# Probability that a randomly placed item will be in the wrong category
+def giniImpurity(rows):
+	total = len(rows)
+	counts = uniqueCounts(rows)
+	imp = 0
+	for k1 in counts:
+		p1 = float(counts[k1]) / total
+
+		for k2 in counts:
+			if k1 == k2:
+				continue
+
+			p2 = float(counts[k2]) / total
+
+			imp += p1 * p2
+
+	return imp
